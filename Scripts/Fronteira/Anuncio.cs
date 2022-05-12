3265c7068cce4900b9f88812a80c18621c0d4d09
Charles Santos <82294630+CharlesSDS@users.noreply.github.com>
2022-05-11 21:18:10 -0300

    Update Anuncio.cs

--- Scripts/Fronteira/Anuncio.cs
+++ Scripts/Fronteira/Anuncio.cs
@@ -30,9 +30,9 @@ namespace Server.Gumps
             this.Resizable = false;
 
             AddPage(0);
-            AddBackground(-1, 27 + y, 537, 25, 9300);
+            AddBackground(-1, 27 + y, 537, 25, 9350);
             AddHtml(28, 30 + y, 421, 22, texto, (bool)false, (bool)false);
-            AddButton(502, 30 + y, 4018, 4024, 0, GumpButtonType.Reply, 0);
+            AddButton(502, 30 + y, 30535, 30535, 0, GumpButtonType.Reply, 0);
             AddItem(-6, 29 + y, 3636);
             from.Anuncios++;
         }
