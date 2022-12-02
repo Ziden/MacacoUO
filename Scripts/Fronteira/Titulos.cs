9cf378cf80d20200f1c026d22e785702e07873fb
Charles Santos <82294630+CharlesSDS@users.noreply.github.com>
2022-12-01 21:27:02 -0300

    Novos Titulos

--- Scripts/Fronteira/Titulos.cs
+++ Scripts/Fronteira/Titulos.cs
@@ -52,6 +52,156 @@ namespace Server.Ziden
         }
     }
 
+    public class TitulosDejabu : BaseRewardTitleDeed
+    {
+        public override TextDefinition Title { get { return new TextDefinition("Matador de Dejabu"); } } // Naughty
+
+        [Constructable]
+        public TitulosDejabu()
+        {
+        }
+
+        public TitulosDejabu(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+        }
+    }
+
+    public class TitulosToppvm : BaseRewardTitleDeed
+    {
+        public override TextDefinition Title { get { return new TextDefinition("Campeão Pvm 2022"); } } // Naughty
+
+        [Constructable]
+        public TitulosToppvm()
+        {
+        }
+
+        public TitulosToppvm(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+        }
+    }
+
+    public class TitulosTopPvP : BaseRewardTitleDeed
+    {
+        public override TextDefinition Title { get { return new TextDefinition("Campeão PvP 2022"); } } // Naughty
+
+        [Constructable]
+        public TitulosTopPvP()
+        {
+        }
+
+        public TitulosTopPvP(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+        }
+    }
+
+    public class TitulosTopTrabalhador : BaseRewardTitleDeed
+    {
+        public override TextDefinition Title { get { return new TextDefinition("Campeão Woker 2022"); } } // Naughty
+
+        [Constructable]
+        public TitulosTopTrabalhador()
+        {
+        }
+
+        public TitulosTopTrabalhador(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+        }
+    }
+
+    public class TitulosLuciferKiller : BaseRewardTitleDeed
+    {
+        public override TextDefinition Title { get { return new TextDefinition("Matador de Lucifer"); } } // Naughty
+
+        [Constructable]
+        public TitulosLuciferKiller()
+        {
+        }
+
+        public TitulosLuciferKiller(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+        }
+    }
+
+    public class Titulospinga : BaseRewardTitleDeed
+    {
+        public override TextDefinition Title { get { return new TextDefinition("O  Pau-d'água"); } } // Naughty
+
+        [Constructable]
+        public Titulospinga()
+        {
+        }
+
+        public Titulospinga(Serial serial)
+            : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+        }
+    }
+
     public class BetaTester : BaseRewardTitleDeed
     {
         public override TextDefinition Title { get { return new TextDefinition("Beta Tester"); } } // Naughty
