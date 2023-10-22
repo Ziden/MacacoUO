bf76055e3c5e5a0d33213d91ce8dac76406a4863
Developer-Morgan <144072956+Developer-Morgan@users.noreply.github.com>
2023-10-21 21:03:02 -0300

    Update Corpse.cs

--- Scripts/Items/Corpses/Corpse.cs
+++ Scripts/Items/Corpses/Corpse.cs
@@ -6,7 +6,7 @@ using Felladrin.Automations;
 using Server.ContextMenus;
 using Server.Engines.PartySystem;
 using Server.Engines.Quests;
-//using Server.Engines.Quests.Doom;
+using Server.Engines.Quests.Doom;
 using Server.Engines.Quests.Haven;
 using Server.Engines.XmlSpawner2;
 using Server.Fronteira.Items.Corda;
@@ -1470,7 +1470,7 @@ namespace Server.Items
                             return;
                         }
                     }
-                  /*  else if (qs is TheSummoningQuest)
+                    else if (qs is TheSummoningQuest)
                     {
                         VanquishDaemonObjective obj = qs.FindObjective(typeof(VanquishDaemonObjective)) as VanquishDaemonObjective;
 
@@ -1491,7 +1491,7 @@ namespace Server.Items
                                 player.SendLocalizedMessage(1050023); // You find a golden skull, but your backpack is too full to carry it.
                             }
                         }
-                    }*/
+                    }
                 }
                 #endregion
 
