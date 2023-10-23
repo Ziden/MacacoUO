7e4016531a34af4ff0956121f016e34ff9ce71c2
Developer-Morgan <144072956+Developer-Morgan@users.noreply.github.com>
2023-10-22 22:35:16 -0300

    Update DarknightCreeper.cs

--- Scripts/Mobiles/Normal/DarknightCreeper.cs
+++ Scripts/Mobiles/Normal/DarknightCreeper.cs
@@ -52,13 +52,13 @@ namespace Server.Mobiles
             DistribuiItem(new CristalDoPoder() { Amount = 5 });
             DistribuiItem(new FragmentosAntigos());
             SorteiaItem(Decos.RandomDeco(this));
-            var joia = Loot.RandomJewelry();
+            /*var joia = Loot.RandomJewelry();
             if (joia.Name != null)
             {
                 joia.Name += " [DOOM]";
             }
             joia.Attributes.SpellDamage = 5;
-            SorteiaItem(joia);
+            SorteiaItem(joia);*/
             GolemMecanico.JorraOuro(c.Location, c.Map, 350);
         }
 
